d406dccd7efd58634834964f156df8b94769f482
Uma Ranjan <30934068+usranjan@users.noreply.github.com>
2021-07-28 13:46:07 +0530

    fixing build errors
    Combined paragraph into single sentence

--- lib/screens/introPage2.dart
+++ lib/screens/introPage2.dart
@@ -131,11 +131,7 @@ class WebViewExampleState extends State<WebViewExample> {
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: Text(
-                  "The Musopathy TBT Health and Wellness App consists of instructional videos of non-strenuous Tonation Breathing Techniques (TBTs) that can help you combat several conditions including the following:
-
- Respiratory issues related to Common Cold, Covid, COPD, Muscular tension, Sinus headaches, Sleep disorders, Stress, Anxiety, Depression, Aggression, Anger, Panic & Phobia and other Behavioral issues.  It also helps in Pain Management and Mind Management by increasing focus, clarity, calmness, energy, stamina, and equanimity. 
-
-The TBT Wellness Videos** are of two types: (a) TBT Techniques such as Nasal Tonation, Buzz Tonation, Pursed Lips Tonation & Pronal Tonation and (b) Applications for various conditions mentioned above.  The duration of each video has been intentionally kept short to enable participants to stay focused and grasp the technique easily and do it themselves thereafter.  
+                  "The Musopathy TBT Health and Wellness App consists of instructional videos of non-strenuous Tonation Breathing Techniques (TBTs) that can help you combat several conditions including Respiratory issues related to Common Cold, Covid, COPD, Muscular tension, Sinus headaches, Sleep disorders, Stress, Anxiety, Depression, Aggression, Anger, Panic & Phobia and other Behavioral issues.  It also helps in Pain Management and Mind Management by increasing focus, clarity, calmness, energy, stamina, and equanimity. The TBT Wellness Videos** are of two types: (a) TBT Techniques such as Nasal Tonation, Buzz Tonation, Pursed Lips Tonation & Pronal Tonation and (b) Applications for various conditions mentioned above.  The duration of each video has been intentionally kept short to enable participants to stay focused and grasp the technique easily and do it themselves thereafter.  
 .",
                   textAlign: TextAlign.justify,
                   style: TextStyle(fontFamily: 'Poppins', fontSize: 18),
